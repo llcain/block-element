ebb107ef4cb5b32ca1bf1f5547201266ebdebc0a
llcain <llcain2012@gmail.com>
2023-12-17 23:14:49 -0600

    Added simple route

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(express.json());
 
+// simple route
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application!!" });
+});
+
 app.listen(5000, () => {
     console.log(`Server started at ${5000}`)
 });
